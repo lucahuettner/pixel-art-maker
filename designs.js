febc2db6203d30d31f2bf7047e906005d68b0c15
Luca Huettner <luca.huettner@gmail.com>
2018-01-06 10:03:10 +0100

    added comments

--- designs.js
+++ designs.js
@@ -3,12 +3,16 @@ let color = document.getElementById('colorPicker');
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-
 document.addEventListener('submit', function makeGrid(event) {
+
+  // preventing that site is reloaded on submit
   event.preventDefault();
+
+  // getting canvas height and width
   const row = document.getElementById('input_height').value;
   const col = document.getElementById('input_width').value;
 
+  // create canvas
   for (let i = 0; i < row; i++){
     document.getElementById('pixel_canvas').appendChild(document.createElement('tr'));
     for (let o = 0; o < col; o++){
