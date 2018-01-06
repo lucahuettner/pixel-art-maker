e15da62ef88acad114df06510b9faf1d82445b0f
Luca Huettner <luca.huettner@gmail.com>
2018-01-06 10:39:54 +0100

    added comments

--- designs.js
+++ designs.js
@@ -17,9 +17,12 @@ document.addEventListener('submit', function makeGrid(event) {
   while (canvas.firstChild) {
     canvas.removeChild(canvas.firstChild)
   }
+
   // create canvas
+  // create rows
   for (let i = 0; i < row; i++){
     canvas.appendChild(document.createElement('tr'));
+    //create columns
     for (let o = 0; o < col; o++){
       canvas.lastChild.appendChild(document.createElement('td'));
     }
